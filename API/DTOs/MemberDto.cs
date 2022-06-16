27676010a593df36a2b4b160519abf03e38ea1be
jthaynes9031@gmail.com <jthaynes9031@gmail.com>
2022-06-16 18:36:13 -0400

    bug Fixes

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,7 +9,7 @@ namespace API.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string PhotoUrl {get;set;}
         public byte[] PasswordHash  { get; set; }
         public byte[] PasswordSalt { get; set; }
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import {TabsModule} from 'ngx-bootstrap/tabs'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import {TabsModule} from 'ngx-bootstrap/tabs'
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   exports: [
     BsDropdownModule,
