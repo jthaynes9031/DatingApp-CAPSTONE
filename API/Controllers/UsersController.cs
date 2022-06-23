598920d4ba154225fc697933ffc26b0a036ee3cf
jthaynes9031@gmail.com <jthaynes9031@gmail.com>
2022-06-23 10:33:37 -0400

    Updates to register Page

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -130,8 +130,6 @@ namespace API.Controllers
             if(await _userRepository.SaveAllAsync()) return Ok();
 
             return BadRequest("failed to delete the photo");
-
-
         }
         
     }
